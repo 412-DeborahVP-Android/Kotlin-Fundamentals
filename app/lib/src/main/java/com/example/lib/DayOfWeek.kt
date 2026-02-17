ae7058b177c4d7d2e2fd40366061358a78d14c9a
deborahvp12 <deborahvillicana@outlook.com>
2026-02-17 16:42:12 -0600

    Un error

--- app/lib/src/main/java/com/example/lib/DayOfWeek.kt
+++ app/lib/src/main/java/com/example/lib/DayOfWeek.kt
@@ -7,16 +7,17 @@ fun main() {
      * */
     val dayOfTheWeek = 2
     when (dayOfTheWeek) {
-            1 -> println("Es lunes")
-            2 -> {
-                println("Es martes")
-                println("Haciendo más cositas")
-            }
-            3 -> println("Es miércoles")
-            4 -> println("Es jueves")
-            5 -> println("Es viernes")
-            6, 7 -> println("Es fin de semana")
-            else -> println("No es un día de la semana")
+        1 -> println("Es lunes")
+        2 -> {
+            println("Es martes")
+            println("Haciendo más cositas")
         }
 
+        3 -> println("Es miércoles")
+        4 -> println("Es jueves")
+        5 -> println("Es viernes")
+        6, 7 -> println("Es fin de semana")
+        else -> println("No es un día de la semana")
+    }
+}shfdksfh
 }
